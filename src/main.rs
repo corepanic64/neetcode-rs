068728041b5866bd1cf8c971f11fd042322ef9c5
Toxir Isakulov <isakulovtohir902@gmail.com>
2026-03-03 12:53:24 +0500

    has duplicate solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let words = vec!["tokhir", "tokhir", "akmal"];
+    let r = has_duplicate(words);
+    println!("{r}")
+}
+
+fn has_duplicate(words: Vec<&str>) -> bool {
+    let mut hm: HashMap<String, usize> = HashMap::new();
+    for (i, v) in words.iter().enumerate() {
+        let hm_word = hm.contains_key(*v);
+        if hm_word {
+            return true;
+        } else {
+            hm.insert(v.to_string(), 1);
+        }
+    }
+    false
 }
